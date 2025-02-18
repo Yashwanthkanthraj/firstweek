658d8a146f8657808bd764e5871b2da999478350
yashwanthkanthraj <yashwanthkanthraj1@gmail.com>
2025-02-18 10:43:36 +0530

    1st program

---
+++ first_pr.java
@@ -0,0 +1,14 @@
+package firstweek.dayone;
+
+/*No spaces: Class names cannot contain spaces."java program"
+which is not allowed.
+
+Class names must start with a letter, underscore (_), or dollar sign ($),
+ and can be followed by letters, digits, underscores, or dollar signs. */
+
+public class first_pr { // class
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
